e0191a940d53c097db5926a36288a31e3daa8401
Sherzod Sobirov <sherzod02sobirov@mail.ru>
2024-11-01 07:38:25 +0300

    footer

--- contact-us.php
+++ contact-us.php
@@ -383,7 +383,7 @@
             navLinks.forEach(link => {
                 link.addEventListener("click", (event) => {
                     event.preventDefault();
-                    const targetId = link.getAttribute("href").substring(1);
+                    const targetI   d = link.getAttribute("href").substring(1);
                     const targetElement = document.getElementById(targetId);
 
                     if (targetElement) {
